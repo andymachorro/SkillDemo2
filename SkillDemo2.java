a98054cbfafa7709bc99a300df54bbb521a6e800
andymachorro <103210240+andymachorro@users.noreply.github.com>
2022-05-01 21:24:18 -0700

    push

--- SkillDemo2.java
+++ SkillDemo2.java
@@ -1,4 +1,7 @@
 public class SkillDemo2{
 
+    public static int add(int a, int b){
+        return a+b;
+    }
 
 }
---
+++ SkillDemoTest2.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest2 {
+    
+    @Test
+    public void add(){
+        assertEquals(4, SkillDemo2.add(2,2));
+    }
+
+}
